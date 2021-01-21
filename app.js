7f4856c87643b88978ebf53559d5c87008c5d5c5
Kiah Pleasant <kiahpleasant@gmail.com>
2021-01-20 20:45:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+// Declare ten different variables and assign them the value of a number.
+var KP1 = 1;
+var KP2 = 2;
+var KP3 = 3;
+var KP4 = 4;
+var KP5 = 5;
+var KP6 = 6;
+var KP7 = 7;
+var KP8 = 8;
+var KP9 = 9;
+var KP10 = 10;
+
+
+// Two times, console.log the addition of two of the variables you declared.
+
+
+console.log(KP1 + KP2);
+console.log(KP5 + KP9);
+
+// Two times, console.log the subtraction of two of the variables you declared.
+
+console.log(KP4 - KP3);
+console.log(KP10 - KP8);
+
+// Two times, console.log the multiplication of two of the variables you declared.
+
+console.log(KP5 * KP7);
+console.log(KP4 * KP6);
+
+// Two times, console.log the division of two of the variables you declared.
+console.log(KP2 / KP1);
+console.log(KP9 / KP3);
+
+//  Two times, increments one of the variables you declared.
+console.log(KP7++);
+console.log(KP2++);
+
+// Two times, decrement one of the variables you have declared.
+console.log(KP8--);
+console.log(KP10--);
